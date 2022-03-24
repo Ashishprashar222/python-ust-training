334ede78ab83608af95b74fe52f3652bc5b0f606
Ashish <ashishprashar37@gmail.com>
2022-03-24 09:07:53 +0530

    Enter

--- raspberry/RPI_BASICS_CODES/buzzer.py
+++ raspberry/RPI_BASICS_CODES/buzzer.py
@@ -5,3 +5,23 @@ GPIO.setwarnings(False)
 GPIO.setup(2,GPIO.OUT)
 GPIO.output(2,GPIO.HIGH)
 print("buzzer on")
+
+
+
+# import RPi.GPIO as GPIO
+# from time import sleep
+# #Disable warnings (optional)
+# GPIO.setwarnings(False)
+# #Select GPIO mode
+# GPIO.setmode(GPIO.BCM)
+# #Set buzzer - pin 23 as output
+# buzzer=23
+# GPIO.setup(buzzer,GPIO.OUT)
+# #Run forever loop
+# while True:
+#  GPIO.output(buzzer,GPIO.HIGH)
+#  print ("Beep")
+#  sleep(0.5) # Delay in seconds
+#  GPIO.output(buzzer,GPIO.LOW)
+#  print ("No Beep")
+#  sleep(0.5)
--- raspberry/RPI_BASICS_CODES/temperature.py
+++ raspberry/RPI_BASICS_CODES/temperature.py
@@ -8,3 +8,4 @@ while True:
         print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
     else:
         print('Failed to get reading. Try again!')
+
